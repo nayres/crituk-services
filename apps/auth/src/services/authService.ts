10f197c0ebf2f33fa78ca3ec4a0a199a91328468
Nick Ayres <naayres@aspeness.com>
2024-12-16 09:07:27 -0700

    Add refresh token logic, WIP

--- apps/auth/src/services/authService.ts
+++ apps/auth/src/services/authService.ts
@@ -8,7 +8,51 @@ import { AxiosInstance } from "axios";
 export class AuthService {
   constructor(private readonly userServiceClient: AxiosInstance) {}
 
-  issueToken = async (clientId: string, clientSecret: string) => {
+  private createAccessToken = ({
+    id,
+    email,
+    first_name,
+    last_name,
+    user_name,
+  }: {
+    id: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    user_name: string;
+  }) => {
+    return jwt.sign(
+      { id, email, first_name, last_name, user_name },
+      process.env.AUTH_SECRET_KEY || "",
+      {
+        expiresIn: process.env.AUTH_ACCESS_TOKEN_EXPIRY,
+      }
+    );
+  };
+
+  private createRefreshToken = ({
+    id,
+    email,
+    first_name,
+    last_name,
+    user_name,
+  }: {
+    id: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    user_name: string;
+  }) => {
+    return jwt.sign(
+      { id, email, first_name, last_name, user_name },
+      process.env.AUTH_REFRESH_SECRET || "",
+      {
+        expiresIn: process.env.AUTH_ACCESS_TOKEN_EXPIRY,
+      }
+    );
+  };
+
+  public issueToken = async (clientId: string, clientSecret: string) => {
     try {
       if (validClients[clientId] && validClients[clientId] === clientSecret) {
         const token = jwt.sign(
@@ -40,7 +84,7 @@ export class AuthService {
     }
   };
 
-  registerUser = async (userData: Partial<IUser>) => {
+  public registerUser = async (userData: Partial<IUser>) => {
     if (typeof userData.password === "undefined") {
       throw new CritukError(
         "Invalid credentials",
@@ -57,32 +101,107 @@ export class AuthService {
     return response;
   };
 
-  authenticateUser = async (userEmail: string, userPassword: string) => {
-    const response = await this.userServiceClient.get(
-      `/users?email=${userEmail}`
-    );
-    const data = response.data;
+  public authenticateUser = async (userEmail: string, userPassword: string) => {
+    try {
+      const response = await this.userServiceClient.get(
+        `/users?email=${userEmail}`
+      );
+      const data = response.data;
 
-    if (!data || !(await bcrypt.compare(userPassword, data.user.password))) {
+      if (!data || !(await bcrypt.compare(userPassword, data.user.password))) {
+        throw new CritukError(
+          "Invalid credentials",
+          ErrorCodes.AUTH.INSUFFICIENT_PERMISSIONS,
+          401
+        );
+      }
+
+      const { id, email, first_name, last_name, user_name } = data.user;
+
+      const accessToken = this.createAccessToken({
+        id,
+        email,
+        first_name,
+        last_name,
+        user_name,
+      });
+
+      const refreshToken = this.createRefreshToken({
+        id,
+        email,
+        first_name,
+        last_name,
+        user_name,
+      });
+
+      return {
+        accessToken,
+        refreshToken,
+      };
+    } catch (error) {
       throw new CritukError(
-        "Invalid credentials",
-        ErrorCodes.AUTH.INSUFFICIENT_PERMISSIONS,
-        401
+        "An unexpected error occured while authenticating user.",
+        ErrorCodes.AUTH.UNEXPECTED_ERROR,
+        500
       );
     }
-
-    const { id, email, first_name, last_name, user_name } = data.user;
-
-    return jwt.sign(
-      { id, email, first_name, last_name, user_name },
-      process.env.AUTH_SECRET_KEY || "",
-      { expiresIn: "1h" }
-    );
   };
 
-  validateToken = async (token: string) => {
+  public issueRefreshToken = async (existingRefreshToken: string) => {
     try {
-      return jwt.verify(token, process.env.AUTH_SECRET_KEY || "");
+      const payload = await this.validateToken(
+        existingRefreshToken,
+        process.env.AUTH_REFRESH_SECRET || ""
+      );
+
+      if (!payload) {
+        throw new CritukError(
+          "Unable to issue refresh token: No JWT payload", // TODO: better error
+          ErrorCodes.AUTH.INSUFFICIENT_PERMISSIONS,
+          403
+        );
+      }
+
+      const { id, email, first_name, last_name, user_name } = payload;
+
+      const refreshToken = this.createRefreshToken({
+        id,
+        email,
+        first_name,
+        last_name,
+        user_name,
+      });
+
+      const accessToken = this.createAccessToken({
+        id,
+        email,
+        first_name,
+        last_name,
+        user_name,
+      });
+
+      return {
+        refreshToken,
+        accessToken,
+      };
+    } catch (error) {
+      throw new CritukError(
+        "An unexpected error occured while issueing a refresh token.",
+        ErrorCodes.AUTH.UNEXPECTED_ERROR,
+        500
+      );
+    }
+  };
+
+  public validateToken = async (token: string, secret: string) => {
+    try {
+      return jwt.verify(token, secret) as {
+        id: string;
+        email: string;
+        first_name: string;
+        last_name: string;
+        user_name: string;
+      };
     } catch (error) {
       throw new CritukError(
         "Invalid token or credentials",
